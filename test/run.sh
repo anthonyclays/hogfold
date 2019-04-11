f416c4b9bd1947853e2e7727c604e02af0d86af1
Felix Obenhuber <felix@obenhuber.de>
2019-04-11 22:02:23 +0200

    Add iottestware dockerfile and script

---
+++ test/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /home/titan/playground/mqtt
+ttcn3_start iottestware.mqtt /config/config.cfg MQTT_TestCases.TC_MQTT_BROKER_CONNECT_001
---
+++ test/run_docker.sh
@@ -0,0 +1,1 @@
+docker run -v `pwd`:/config  -ti testware "/config/run.sh"
